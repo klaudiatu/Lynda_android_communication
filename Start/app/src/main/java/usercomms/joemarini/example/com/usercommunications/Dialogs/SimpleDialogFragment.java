f2b9b4896a8fc9446820affec2aa5b77ebb39a83
klaudiatu <klaudia.turemka@o2.pl>
2018-07-09 11:08:05 +0200

    Update SimpleDialogFragment.java

--- Start/app/src/main/java/usercomms/joemarini/example/com/usercommunications/Dialogs/SimpleDialogFragment.java
+++ Start/app/src/main/java/usercomms/joemarini/example/com/usercommunications/Dialogs/SimpleDialogFragment.java
@@ -17,18 +17,47 @@ public class SimpleDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         //TODO: Create an AlertDialog.Builder instance
+        AlertDialor.Builder builder = new AlertDialog.Builder(getActivity());
 
 
         //TODO: Set builder properties
-
+        builder.setTitle("Peas Preference");
+        builder.setMessage("Do you like sugar snap peas?");
+        
+        builder.setPositiveButton("Sure!", new DialogInterface.OnClickListener() {
+        @Override
+            public void onClick(DialogInterface dialog, int which){
+            Log.i(TAG, "Positive button clicked");
+            }
+        
+        });
+            
+        builder.setNegativeButton("No Way!", new DialogInterface.OnClickListener() {
+        @Override
+            public void onClick(DialogInterface dialog, int which){
+            Log.i(TAG, "Negative button clicked");
+            }
+        
+        });
+        
+        builder.setNeutralButton("Not sure", new DialogInterface.OnClickListener() {
+        @Override
+            public void onClick(DialogInterface dialog, int which){
+            Log.i(TAG, "Neutral button clicked");
+            }
+        
+        });
 
         // TODO: return the created dialog
-        return null;
+        return builder.create();
     }
 
-    // TODO: Listen for cancel message by overriding onCancel
-
-
+    // TODO: Listen for cancel message by overriding onCancel    
+    @Override
+    public void onCancel(DialogInterface dlg){
+    super.onCancel(dlg);
+        Log.i(TAG, "Dialog cancelled")
+    }
     // TODO: Override onAttach to get Activity instance
 
 }
